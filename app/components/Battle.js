a3ec29da51dfb2b0eade27246156d4f81f5a5d1c
seanhallahan <sean.hallahan@accenturefederal.com>
2017-08-26 15:06:46 -0400

    completed results and loading components

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -1,36 +1,7 @@
 var React = require('react');
 var PropTypes = require('prop-types');
 var Link = require('react-router-dom').Link;
-
-function PlayerPreview (props) {
-  return (
-    <div>
-      <div className='column'>
-        <img
-          className='avatar'
-          src={props.avatar}
-          alt={'Avatar for '+  props.username} />
-        <h2 className='username'>@{props.username}</h2>
-        <button
-          className='reset'
-          onClick={props.onReset.bind(null, props.id)}>
-          Reset
-        </button>
-
-      </div>
-    </div>
-  )
-}
-
-PlayerPreview.propTypes = {
-  avatar: PropTypes.string.isRequired,
-  username: PropTypes.string.isRequired,
-  id: PropTypes.string.isRequired,
-  onReset: PropTypes.func.isRequired
-}
-
-
-
+var PlayerPreview = require('./PlayerPreview');
 
 class PlayerInput extends React.Component {
   constructor(props) {
@@ -145,11 +116,16 @@ class Battle extends React.Component {
               onSubmit={this.handleSubmit}
             />
             : <PlayerPreview
-                id='playerOne'
                 username={playerOneName}
                 avatar={playerOneImage}
-                onReset={this.handleReset}
-            />}
+            >
+              <button
+                className='reset'
+                onClick={this.handleReset.bind(null, 'playerOne')}>
+                Reset
+              </button>
+            </PlayerPreview>
+          }
 
           {!playerTwoName
             ? <PlayerInput
@@ -158,11 +134,15 @@ class Battle extends React.Component {
               onSubmit={this.handleSubmit}
             />
             : <PlayerPreview
-                id='playerTwo'
                 username={playerTwoName}
                 avatar={playerTwoImage}
-                onReset={this.handleReset}
-            />}
+            >
+              <button
+                className='reset'
+                onClick={this.handleReset.bind(null, 'playerTwo')}>
+                Reset
+              </button>
+            </PlayerPreview>}
         </div>
         {playerOneImage && playerTwoImage &&
           <Link
--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,6 +1,7 @@
 var React = require('react');
 var PropTypes = require('prop-types');
 var api = require('../utils/api');
+var Loading = require('./Loading');
 
 function RepoGrid (props) {
   return (
@@ -94,7 +95,7 @@ class Popular extends React.Component {
           onSelect={this.updateLanguage}
         />
         {!this.state.repos
-          ? <p>LOADING</p>
+          ? <Loading text={"Loading Repos for" + this.state.selectedLanguage} speed="200"/>
           : <RepoGrid repos={this.state.repos} />}
       </div>
     )
